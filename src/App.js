45f8aaa9924dbafc7b29df3466dadb3d58ae7d55
Joe Powell <joe_773@yahoo.com>
2021-08-27 16:08:47 -0500

    mgg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("")    // Keep track of current todo we are adding
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    }
+    console.log(todos)
+    setTodos([...todos].concat(newTodo))
+    setTodo('')
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={(e) => setTodo(e.target.value)} value={todo} />
+        <button type='submit'>Add</button>
+      </form>
+      {todos.map((item, i) => <div key={i}>{item.text}</div>)}
     </div>
   );
 }
