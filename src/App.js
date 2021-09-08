d173541139038c9cd4f10d01693a3bda1443a697
Joe Powell <joe_773@yahoo.com>
2021-09-08 09:50:30 -0500

    mgg

--- src/App.js
+++ src/App.js
@@ -5,43 +5,84 @@ function App() {
 
   const [todos, setTodos] = useState([])
   const [todo, setTodo] = useState("")    // Keep track of current todo we are adding
+  //todo is current typing, so bottom sets top
+  const [todoEditing, setTodoEditing] = useState(null)
+  const [editingText, setEditingText] = useState('')
+  // top sets bottom here
+
 
 
   function handleSubmit(e) {
     e.preventDefault();
+
     const newTodo = {
       id: new Date().getTime(),
       text: todo,
       completed: false,
     }
+
     console.log(todos)
     setTodos([...todos, newTodo])
     setTodo('')
   }
 
 
-// 18:00
+
+
   function deleteTodo(id) {
      console.log(id)             
-       // return only the values where the item.id is !== not equal to id. if false value it will not return that element in the array
-   const updatedTodos = todos.filter((item) => 
-   
-      item.id !== id
-   
-    )
+      
+   const updatedTodos = todos.filter((item) => item.id !== id)
     setTodos(updatedTodos)
   }
 
 
+
+  function toggleComplete(id) {
+
+      const updatedTodos = [...todos].map((item) => {
+        if(item.id === id) {
+          item.completed = !item.completed  
+        }
+        return item;
+      })
+
+      setTodos(updatedTodos)
+
+  }
+
+
+
+
   return (
     <div className="App">
+
       <form onSubmit={handleSubmit}>
         <input type='text' onChange={(e) => setTodo(e.target.value)} value={todo} />
         <button type='submit'>Add</button>
       </form>
-      {todos.map((item) => <div key={item.id} >
-        <div>{item.text}</div>
-        <button onClick={() => deleteTodo(item.id)}>Delete</button>
+
+      {todos.map((item) => 
+        <div key={item.id} >     
+          
+         
+          {todoEditing === item.id ? (<input type='text' 
+            onChange={(e) => setEditingText(e.target.value) } 
+             value={editingText} /> )
+            : (<div>{item.text}</div>)}
+          
+
+          
+
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
+          <input type='checkbox' 
+            onChange={() => toggleComplete(item.id) } 
+            checked={item.completed} 
+          />
+
+          <button onClick={() => setTodoEditing(item.id)}>Edit</button>
+          <button onClick={() => editTodo(item.id) }>Submit Edits</button>
+
         </div>)}
     </div>
   );
